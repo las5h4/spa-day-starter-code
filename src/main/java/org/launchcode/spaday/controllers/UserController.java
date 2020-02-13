208c7f6247d4dd6fffffeff7baeacd5ef2f27798
las5h4 <loganasmalley@gmail.com>
2020-02-13 12:39:00 -0600

    Completed bonus missions - added data layer, id functionality, date functionality, user detail page

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,12 +1,10 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.data.UserData;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
@@ -21,20 +19,25 @@ public class UserController {
     public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
         // add form submission handling code here
         if ((user.getPassword()).equals(verify)) {
-            model.addAttribute(user);
+            model.addAttribute("thisUser", user);
+            model.addAttribute("users", UserData.getAll());
+            UserData.add(user);
             return "user/index";
         }
         else {
-            String passwordError;
-            String prevUsername;
-            String prevEmail;
-            prevUsername = user.getUsername();
-            prevEmail = user.getEmail();
-            passwordError = "Passwords did not match. Try again!";
+            String passwordError = "Passwords did not match. Try again!";
+            String prevUsername = user.getUsername();
+            String prevEmail = user.getEmail();
             model.addAttribute("passwordError", passwordError);
             model.addAttribute("prevUsername", prevUsername);
             model.addAttribute("prevEmail", prevEmail);
             return "user/add";
         }
+    }
+
+    @GetMapping("{id}")
+    public String displayUserDetail(Model model, @PathVariable int id){
+        model.addAttribute("user", UserData.getById(id));
+        return "user/detail";
     }
 }
